73ae912cb5d6292056acc1c821fe486a54531f71
Gerold55 <marleybeare56@gmail.com>
2018-04-25 22:49:31 -0400

    Add files via upload

---
+++ init.lua
@@ -0,0 +1,318 @@
+--[[level1 = 100
+level1_drop = "default:steelblock 10"
+]]
+
+minetest.override_item("default:stone_with_coal", {
+	groups = {cracky=3, xp=1},
+})
+
+minetest.override_item("default:stone_with_iron", {
+	groups = {cracky=2, xp=1},
+})
+
+minetest.override_item("default:stone_with_copper", {
+	groups = {cracky=2, xp=1},
+})
+
+minetest.override_item("default:stone_with_mese", {
+	groups = {cracky=1, xp=2},
+})
+
+minetest.override_item("default:stone_with_gold", {
+	groups = {cracky=2, xp=2},
+})
+
+minetest.override_item("default:stone_with_diamond", {
+	groups = {cracky=1, xp=3},
+})
+
+if minetest.get_modpath("moreores") ~= nil then
+minetest.override_item("moreores:mineral_tin", {
+	groups = {cracky=3, xp=1},
+})
+minetest.override_item("moreores:mineral_silver", {
+	groups = {cracky=3, xp=2},
+})
+minetest.override_item("moreores:mineral_mithril", {
+	groups = {cracky=3, xp=3},
+})
+end
+
+if minetest.get_modpath("technic") ~= nil then
+minetest.override_item( "technic:mineral_uranium", {
+	groups = {cracky=3, radioactive=1000,xp=3},
+}) 
+
+minetest.override_item( "technic:mineral_chromium", {
+	groups = {cracky=3,xp=2},
+}) 
+
+minetest.override_item( "technic:mineral_zinc", {
+	groups = {cracky=3, xp=1},
+})
+end
+
+local players = {}
+
+MAX_HUD_EXPERIENCE = 100
+
+minetest.register_on_joinplayer(function(player)
+	local playerName = player:get_player_name()
+	players[playerName] = {
+		
+		experiencebar = 0,
+		experiencetotal = 0,
+		textnumber = 0,
+		exphudnumber = 0,
+		hud = player:hud_add({
+			hud_elem_type = "statbar",
+			texture = ("xp_blank"),
+			position = {x=0.5,y=1},
+			size = {x=20, y=20},
+			text = "orb.png",
+			number = 40,
+			alignment = {x=0,y=1},
+			offset = {x=-200, y=-87},
+		      }
+		),
+		
+	
+		hud2 = player:hud_add({
+		    hud_elem_type = "text",
+                    name = "player_hud:time",
+					texture = ("xp_blank"),
+                    position = {x=0.5,y=1},
+                    text = "",
+                    scale = {x=2,y=2},
+                    alignment = {x=0,y=1},
+                    number = 0xFF0000, --0xFFFFFF,
+		    offset = {x=-160 , y=-65},
+			}
+		),
+	}
+end)
+
+minetest.register_on_leaveplayer(function(player)
+	local playerName = player:get_player_name()
+	players[playerName] = nil
+end)
+
+
+--add an experience orb if player digs node from xp group
+minetest.register_on_dignode(function(pos, oldnode, digger)
+	local namer = oldnode.name
+	local see_if_mineral = minetest.get_item_group(namer, "xp")
+	if see_if_mineral == 1 then
+		minetest.env:add_entity(pos, "experience:orb")
+	end
+	if see_if_mineral == 2 then
+		minetest.env:add_entity(pos, "experience:orb")
+		minetest.env:add_entity(pos, "experience:orb")
+	end
+	if see_if_mineral == 3 then
+	minetest.env:add_entity(pos, "experience:orb")
+	minetest.env:add_entity(pos, "experience:orb")
+	minetest.env:add_entity(pos, "experience:orb")
+	end
+	if see_if_mineral == 4 then
+	minetest.env:add_entity(pos, "experience:orb")
+	minetest.env:add_entity(pos, "experience:orb")
+	minetest.env:add_entity(pos, "experience:orb")
+	minetest.env:add_entity(pos, "experience:orb")
+	end
+	if see_if_mineral == 6 then
+	minetest.env:add_entity(pos, "experience:orb")
+	minetest.env:add_entity(pos, "experience:orb")
+	minetest.env:add_entity(pos, "experience:orb")
+	minetest.env:add_entity(pos, "experience:orb")
+	minetest.env:add_entity(pos, "experience:orb")
+	minetest.env:add_entity(pos, "experience:orb")
+	end
+	if see_if_mineral == 8 then
+	minetest.env:add_entity(pos, "experience:orb")
+	minetest.env:add_entity(pos, "experience:orb")
+	minetest.env:add_entity(pos, "experience:orb")
+	minetest.env:add_entity(pos, "experience:orb")
+	minetest.env:add_entity(pos, "experience:orb")
+	minetest.env:add_entity(pos, "experience:orb")
+	minetest.env:add_entity(pos, "experience:orb")
+	minetest.env:add_entity(pos, "experience:orb")
+	end
+end)
+--give a new player some xp
+minetest.register_on_newplayer(function(player)
+	local file = io.open(minetest.get_worldpath().."/"..player:get_player_name().."_experience", "w")
+	file:write("0")
+	file:close()
+end)
+--set player's xp level to 0 if they die
+minetest.register_on_dieplayer(function(player)
+	local file = io.open(minetest.get_worldpath().."/"..player:get_player_name().."_experience", "w")
+	file:write("0")
+	file:close()
+end)
+
+--Allow people to collect orbs
+minetest.register_globalstep(function(dtime)
+
+
+local gameTime = minetest.get_gametime()
+
+
+	for _,player in ipairs(minetest.get_connected_players()) do
+		local pos = player:getpos()
+		pos.y = pos.y+0.5
+		for _,object in ipairs(minetest.env:get_objects_inside_radius(pos, 1)) do
+			if not object:is_player() and object:get_luaentity() and object:get_luaentity().name == "experience:orb" then
+				--RIGHT HERE ADD IN THE CODE TO UPGRADE PLAYERS 
+				object:setvelocity({x=0,y=0,z=0})
+				object:get_luaentity().name = "STOP"
+				minetest.sound_play("orb", {
+					to_player = player:get_player_name(),
+				})
+				
+				if io.open(minetest.get_worldpath().."/"..player:get_player_name().."_experience", "r") == nil then
+				local file = io.open(minetest.get_worldpath().."/"..player:get_player_name().."_experience", "w")
+				file:write("1")
+				local experience = 1
+				file:close()
+				else  
+				  
+				local xp = io.open(minetest.get_worldpath().."/"..player:get_player_name().."_experience", "r")
+				local experience = xp:read("*l")
+				xp:close()
+				
+				if experience ~= nil then
+					local new_xp = experience + 1
+					local xp_write = io.open(minetest.get_worldpath().."/"..player:get_player_name().."_experience", "w")
+					xp_write:write(new_xp)
+					xp_write:close()
+					--[[if new_xp == level1 then
+						minetest.env:add_item(pos, level1_drop)
+						minetest.sound_play("level_up", {
+							to_player = player:get_player_name(),
+						})
+					end
+					]]
+				end
+				
+				end
+				
+				object:remove()
+			end
+		end
+		for _,object in ipairs(minetest.env:get_objects_inside_radius(pos, 3)) do
+			if not object:is_player() and object:get_luaentity() and object:get_luaentity().name == "experience:orb" then
+				if object:get_luaentity().collect then
+					local pos1 = pos
+					pos1.y = pos1.y+0.2
+					local pos2 = object:getpos()
+					local vec = {x=pos1.x-pos2.x, y=pos1.y-pos2.y, z=pos1.z-pos2.z}
+					vec.x = vec.x*3
+					vec.y = vec.y*3
+					vec.z = vec.z*3
+					object:setvelocity(vec)
+				end
+			end
+		end
+		
+				--Loop through all connected players
+		for playerName,playerInfo in pairs(players) do
+			local player = minetest.get_player_by_name(playerName)
+			if player ~= nil then
+				
+				if playerInfo["experiencetotal"] == nil then
+				  playerInfo["experiencetotal"] = 0
+				end
+				
+		--[[
+		local xptemp = io.open(minetest.get_worldpath().."/"..player:get_player_name().."_experience", "r")
+		if xptemp ~= nil then
+		local xptemp2 = xptemp:read("*l")
+		end
+		if xptemp2 == nil then
+		      local file = io.open(minetest.get_worldpath().."/"..player:get_player_name().."_experience", "w")	
+		      file:write("0")
+		      playerInfo["experiencetotal"] = 0
+		      playerInfo["textnumber"] = 0
+		      file:close()
+		end
+		]]
+		local xptemp = io.open(minetest.get_worldpath().."/"..player:get_player_name().."_experience", "r")
+		if xptemp ~= nil then
+		local xptemp2 = xptemp:read("*l")
+		      if xptemp2 ~= nil then
+			    playerInfo["experiencetotal"] = xptemp2
+		      end
+		end
+		if xptemp ~= nil then
+		xptemp:close()
+		end
+
+
+				if (playerInfo["experiencetotal"]) ~= nil then
+					playerInfo["experiencebar"] = playerInfo["experiencetotal"] - 0
+					
+					playerInfo["textnumber"] = 0
+				
+				    while playerInfo["experiencebar"] >= MAX_HUD_EXPERIENCE do
+					  playerInfo["textnumber"]= playerInfo["textnumber"] + 1
+					  playerInfo["experiencebar"] = playerInfo["experiencebar"] - MAX_HUD_EXPERIENCE
+				    end
+				end
+
+				
+				--Update the players's hud xp bar
+				
+				player:hud_change(playerInfo["hud"], "number", playerInfo["experiencebar"])
+				player:hud_change(playerInfo["hud2"], "text", playerInfo["textnumber"])
+				
+			end
+		end
+		
+	end
+end)
+
+minetest.register_entity("experience:orb", {
+	physical = true,
+	timer = 0,
+	textures = {"orb.png"},
+	visual_size = {x=0.15, y=0.15},
+	collisionbox = {-0.17,-0.17,-0.17,0.17,0.17,0.17},
+	on_activate = function(self, staticdata)
+		self.object:set_armor_groups({immortal=1})
+		self.object:setvelocity({x=0, y=1, z=0})
+		self.object:setacceleration({x=0, y=-10, z=0})
+	end,
+	collect = true,
+	on_step = function(self, dtime)
+		self.timer = self.timer + dtime
+		if (self.timer > 300) then
+			self.object:remove()
+		end
+		local p = self.object:getpos()
+		local nn = minetest.env:get_node(p).name
+		local noder = minetest.env:get_node(p).name
+		p.y = p.y - 0.3
+		local nn = minetest.env:get_node(p).name
+		if not minetest.registered_nodes[nn] or minetest.registered_nodes[nn].walkable then
+			if self.physical_state then
+				self.object:setvelocity({x=0, y=0, z=0})
+				self.object:setacceleration({x=0, y=0, z=0})
+				self.physical_state = false
+				self.object:set_properties({
+					physical = false
+				})
+			end
+		else
+			if not self.physical_state then
+				self.object:setvelocity({x=0,y=0,z=0})
+				self.object:setacceleration({x=0, y=-10, z=0})
+				self.physical_state = true
+				self.object:set_properties({
+					physical = true
+				})
+			end
+		end
+	end,
+})
